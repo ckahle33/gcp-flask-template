07edf77a4c358ed626463ba1badf7b8262ddc916
cpk <ckahle33@gmail.com>
2020-09-06 11:23:24 -0500

    fix requirements

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ install python3 from https://www.python.org/downloads/
 ```
 python3 -m venv venv # creates a directory in ./ where all modules live
 source venv/bin/activate # activate the virtural environment
-pip -r install requirements.txt # grabs modules
+pip install -r requirements.txt # grabs modules
 export FLASK_APP=app.py
 flask run
 ```
--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cpk@localhost/hackforgood'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 @app.route('/')
 def index():
