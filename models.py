83e5656d5598593477095d00df47b6905a9403dd
cpk <ckahle33@gmail.com>
2020-09-07 00:38:31 -0500

    login flow, templating

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
-from app import db
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-
+# from app import db
+#
+# db = SQLAlchemy(app)
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     auth_token = db.Column(db.String(120), unique=True, nullable=True)
+#     avatar_url = db.Column(db.String(200), unique=True, nullable=True)
+#     location = db.Column(db.String(200), unique=False, nullable=True)
