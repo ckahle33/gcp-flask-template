7f6a4f0cf6239afb053633a1d3af54a8be0eaefa
cpk <ckahle33@gmail.com>
2020-11-29 17:37:09 -0600

    fix boot script

--- boot.sh
+++ boot.sh
@@ -5,8 +5,9 @@
 python3 -m venv venv # creates a directory in ./ where all modules live
 source venv/bin/activate # activate the virtural environment
 pip install -r requirements.txt # grabs modules
-psql -c 'CREATE DATABASE testdb'
-echo "DATABASE_URI='postgres://postgres@localhost/testdb'" >> .env2
+psql -c 'CREATE DATABASE hackforgood'
+echo -e "DATABASE_URI='postgres://postgres@localhost/hackforgood'" >> .env2
 export FLASK_APP=app.py
+flask db upgrade
 flask seed
 flask run
