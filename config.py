70a7438a2deba634445d8e684db30953ced19377
Craig Kahle <kahle@sxsw.com>
2020-12-21 19:28:50 -0600

    db connection fixes

--- config.py
+++ config.py
@@ -9,25 +9,25 @@ class Config(object):
     DB_USER=os.getenv("DB_USER")
     DB_PASSWORD=os.getenv("DB_PASS")
     DB_DATABASE=os.getenv("DB_NAME")
-    DB_CONNECTION_NAME = os.getenv("DB_CONNECTION")
+    DB_CONNECTION = os.getenv("DB_CONNECTION")
     LOCAL_SQLALCHEMY_DATABASE_URI = (
-        'postgresql+psycopg2://{nam}:{pas}@127.0.0.1:5432/{dbn}').format (
-        nam=DB_USER,
-        pas=DB_PASSWORD,
-        dbn=DB_DATABASE,
+        'postgresql+psycopg2://{db_user}:{db_password}@127.0.0.1:5432/{db_name}').format (
+        db_user=DB_USER,
+        db_password=DB_PASSWORD,
+        db_name=DB_DATABASE,
     )
 
     LIVE_SQLALCHEMY_DATABASE_URI = (
-        'postgresql+psycopg2://{nam}:{pas}@localhost/{dbn}?host=/cloudsql/{con}').format (
-        nam=DB_USER,
-        pas=DB_PASSWORD,
-        dbn=DB_DATABASE,
-        con=DB_CONNECTION_NAME,
+        'postgresql+psycopg2://{db_user}:{db_password}@localhost/{db_name}?host=/cloudsql/{db_conn}').format (
+        db_user=DB_USER,
+        db_password=DB_PASSWORD,
+        db_name=DB_DATABASE,
+        db_conn=DB_CONNECTION,
     )
 
-    if os.getenv('GAE_INSTANCE'):
-        SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
-    else:
+    if os.getenv('GAE_INSTANCE') == None:
         SQLALCHEMY_DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
+    else:
+        SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
